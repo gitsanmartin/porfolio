6aed8a0f7729ddb25d10bffa4a3ce27a990116f6
Adrián San Martín <sanmartin.adrian@gmail.com>
2024-03-21 09:55:29 -0300

    add github page simple deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://gitsanmartin.github.io',
+    base: 'porfolio',
+});
