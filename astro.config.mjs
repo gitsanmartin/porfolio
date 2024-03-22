3e49a93ead1ea8be65c68c2fc2924655681297f2
Adrián San Martín <sanmartin.adrian@gmail.com>
2024-03-22 11:32:32 -0300

    change githubpage for vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://gitsanmartin.github.io',
-    base: 'porfolio',
+  output: "server",
+  adapter: vercel()
 });
